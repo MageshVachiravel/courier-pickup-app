4e97c1283af5dd5c6799b2cf570842e775178aad
MageshVachiravel <mageshvachiravel@gmail.com>
2024-02-02 19:30:07 +0530

    feat: displayed the userDetails in UI

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ form.addField("#pickup-area",[
 ])
 
 
+
 form.onSuccess((e) => {
 
     const formData = new FormData(formEl);
@@ -58,13 +59,13 @@ form.onSuccess((e) => {
     const existingUserDetails = localStorage.getItem("userDetails");
 
     if(existingUserDetails){
-        // parse the value into obj/arr
-        const existingUserObj = JSON.parse(existingUserDetails);
-    
-        // push the value to created obj
-        existingUserObj.push(formValueObj)
 
-        localStorage.setItem("userDetails",JSON.stringify(existingUserObj))
+        const existingUserArr = JSON.parse(existingUserDetails);
+    
+        existingUserArr.push(formValueObj)
+
+        localStorage.setItem("userDetails",JSON.stringify(existingUserArr))
+
     }else{
         newUserDetails.push(formValueObj)
 
@@ -75,3 +76,36 @@ form.onSuccess((e) => {
 
     formEl.reset()
 })
+
+
+function getAllCourierDatas(){
+    // get the data from local storage
+    const userData = localStorage.getItem("userDetails");
+    const userDataArr = JSON.parse(userData);
+
+    const userCount = document.querySelector("#courierCount")
+    userCount.innerHTML = userDataArr.length;
+
+    console.log(userDataArr)
+
+    // write those data in ui
+    const tableBody = document.querySelector("#courierDataTable")
+
+    userDataArr.map((data,index)=>{
+        const newTr = `
+            <tr>
+            <td class="px-2 py-1 border">${index+1}</td>
+            <td class="px-2 py-1 border">${data.name}</td>
+            <td class="px-2 py-1 border">${data.mobile}</td>
+            <td class="px-2 py-1 border">${data['pickup-date']}</td>
+            <td class="px-2 py-1 border">${data['pickup-area']}</td>
+            <td class="px-2 py-1 border"><button class="bg-red-500 text-white px-2 py-1 rounded text-sm hover:bg-red-600">Delete</button></td>
+            </tr>
+    `
+
+    tableBody.innerHTML += newTr;
+    })
+    
+}
+
+getAllCourierDatas()
